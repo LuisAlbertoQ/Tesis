832b5314c52e012fd4400ed71b893e3099a6f79c
LuisAlbertoQ <103071836+LuisAlbertoQ@users.noreply.github.com>
2024-06-05 21:40:58 -0500

    confirServe,configData

--- ms-Tesistas/src/main/java/com/example/mstesistas/MsTesistasApplication.java
+++ ms-Tesistas/src/main/java/com/example/mstesistas/MsTesistasApplication.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class MsTesistasApplication {
